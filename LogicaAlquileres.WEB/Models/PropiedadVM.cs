81d3787b6cbfcd32dc31606bd0a4c3a59fa18faf
Brian Cossio <briancossio_96@hotmail.com>
2024-10-13 08:59:52 -0300

    agregue la parte de views para ver la lista

--- LogicaAlquileres.WEB/Models/PropiedadVM.cs
+++ LogicaAlquileres.WEB/Models/PropiedadVM.cs
@@ -1,6 +1,14 @@
-﻿namespace LogicaAlquileres.WEB.Models
+﻿using LogicaAlquileres.Managers.Entidades;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace LogicaAlquileres.WEB.Models
 {
     public class PropiedadVM
     {
+        public Propiedad model { get; set; } //agregue esto
+
+        public List<SelectListItem> ListaEstadosItem { get; set; }//agregue esto
+
+
     }
 }
